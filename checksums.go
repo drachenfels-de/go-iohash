4198953eefb8114ff937830ba41955506fa66a8c
Ruben Jenster <r.jenster@drachenfels.de>
2019-11-06 15:27:42 +0100

    Implement function to lookup a checksum in a checksum file.

---
+++ checksums.go
@@ -0,0 +1,50 @@
+package iohash
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"hash"
+	"io"
+	"os"
+	"strings"
+)
+
+var ErrMismatch = errors.New("checksum mismatch")
+var ErrNotFound = errors.New("no checksum found")
+
+func PrintHash(w io.Writer, h hash.Hash, filename string) {
+	fmt.Fprintf(w, "%x %s\n", h.Sum(nil), filename)
+}
+
+// See description of iohash.Check.
+func CheckFile(checksumsFile string, h hash.Hash, filename string) error {
+	f, err := os.Open(checksumsFile)
+	if err != nil {
+		return fmt.Errorf("failed to open checksums file %s: %s", checksumsFile, err)
+	}
+	defer f.Close()
+	return Check(f, h, filename)
+}
+
+// Check verifies the checksum of targetFile.
+// The hash from the first line matching the targetFile is checked.
+// Any succeeding lines are ignored.
+// The line format is the output from #PrintHash, which is
+// similar to the unix coreutils commands e.g `sha1` `sha512` ...
+func Check(rdr io.Reader, h hash.Hash, targetFile string) error {
+	sc := bufio.NewScanner(rdr)
+	hashS := fmt.Sprintf("%x", h.Sum(nil))
+	for sc.Scan() {
+		line := sc.Text()
+		fileName := strings.TrimSpace(line[len(hashS):])
+		checksum := line[:len(hashS)]
+		if targetFile == fileName {
+			if hashS == checksum {
+				return nil
+			}
+			return ErrMismatch
+		}
+	}
+	return ErrNotFound
+}
--- iohash_test.go
+++ iohash_test.go
@@ -3,8 +3,10 @@ package iohash
 import (
 	"bytes"
 	"crypto/sha1"
-	"github.com/stretchr/testify/assert"
+	"crypto/sha512"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 )
@@ -18,15 +20,39 @@ func TestReader(t *testing.T) {
 	in := strings.NewReader(content)
 	r := NewReader(in, sha1.New())
 	readContent, _ := ioutil.ReadAll(r)
-	assert.Equal(t, content, string(readContent))
-	assert.Equal(t, contentSHA1Sum, StringOfHash(r.Hash))
+	require.Equal(t, content, string(readContent))
+	require.Equal(t, contentSHA1Sum, StringOfHash(r.Hash))
 }
 
 func TestWriter(t *testing.T) {
 	buf := bytes.NewBuffer(nil)
 	w := NewWriter(buf, sha1.New())
 	_, err := w.Write([]byte(content))
-	assert.NoError(t, err)
-	assert.Equal(t, content, buf.String())
-	assert.Equal(t, contentSHA1Sum, StringOfHash(w.Hash))
+	require.NoError(t, err)
+	require.Equal(t, content, buf.String())
+	require.Equal(t, contentSHA1Sum, StringOfHash(w.Hash))
+}
+
+func TestCheck(t *testing.T) {
+	h1 := sha512.New()
+	h1.Write([]byte("hello world"))
+	h2 := sha512.New()
+	h2.Write([]byte("foobar"))
+	h3 := sha512.New()
+	h2.Write([]byte("foobar"))
+	checksums, err := ioutil.TempFile("", "")
+	require.NoError(t, err)
+	PrintHash(checksums, h1, "file1")
+	PrintHash(checksums, h2, "file2")
+	PrintHash(checksums, h3, "file3")
+	checksums.Close()
+	defer os.Remove(checksums.Name())
+
+	// read SHA512 sums from the FILEs and check them
+	require.NoError(t, CheckFile(checksums.Name(), h1, "file1"))
+	require.NoError(t, CheckFile(checksums.Name(), h2, "file2"))
+	require.NoError(t, CheckFile(checksums.Name(), h3, "file3"))
+	require.Equal(t, ErrMismatch, CheckFile(checksums.Name(), sha512.New(), "file2"))
+	require.Equal(t, ErrNotFound, CheckFile(checksums.Name(), h3, "file4"))
+	require.Error(t, CheckFile("doesnotexist", h3, "file3"))
 }
